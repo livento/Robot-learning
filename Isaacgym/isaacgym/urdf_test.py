1bed4a4f3dbd2392bc71278e236037b889ce5741
leovento <livento08@gmail.com>
2023-12-08 23:22:41 +0800

    urdf更改

--- Isaacgym/isaacgym/urdf_test.py
+++ Isaacgym/isaacgym/urdf_test.py
@@ -43,9 +43,9 @@ def set_plane(plane_params):
 
 #添加URDF路径
 asset_root = "/home/leovento/Robot-learning/urdf"
-asset_file = "zongzhuangURDF5/urdf/zongzhuangURDF5.urdf"
+#asset_file = "zongzhuangURDF5/urdf/zongzhuangURDF5.urdf"
 #asset_file = "lm2/urdf/lm2.urdf"
-#asset_file = "cassie/urdf/cassie.urdf"
+asset_file = "cassie/urdf/cassie.urdf"
 
 gym = gymapi.acquire_gym()
 
@@ -97,7 +97,7 @@ for i in range(num_envs):
     height = random.uniform(1.0, 2.5)
 
     initial_pose = gymapi.Transform()
-    initial_pose.p = gymapi.Vec3(0.0, 0, 0)  #每个actor加入时的位置
+    initial_pose.p = gymapi.Vec3(0.0, 0, 1.060)  #每个actor加入时的位置
     #initial_pose.r = gymapi.Quat(-0.707107, 0, 0, 0.707107) #四元组位姿，因为isaacgym是基于y轴向上设计的，因此导入z轴向上的模型时需要进行旋转
     #initial_pose.r = gymapi.Quat(1, 0, 0, 0)
     #为每一个环境添加对象
